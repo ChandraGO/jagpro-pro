6641cfcde4c825312945b22d72137e64d4ac2bc1
Chandra Hafi <haficdh@gmail.com>
2025-11-13 20:47:15 +0800

    Update otomatis 13/11/2025 20:47:15,18

--- lib/Utils/wileys-event-stream.d.ts
+++ lib/Utils/wileys-event-stream.d.ts
@@ -1,16 +1,16 @@
-import type { BaileysEventEmitter } from '../Types';
+import type { WileysEventEmitter } from '../Types';
 /**
- * Captures events from a baileys event emitter & stores them in a file
+ * Captures events from a wileys event emitter & stores them in a file
  * @param ev The event emitter to read events from
  * @param filename File to save to
  */
-export declare const captureEventStream: (ev: BaileysEventEmitter, filename: string) => void;
+export declare const captureEventStream: (ev: WileysEventEmitter, filename: string) => void;
 /**
  * Read event file and emit events from there
  * @param filename filename containing event data
  * @param delayIntervalMs delay between each event emit
  */
 export declare const readAndEmitEventStream: (filename: string, delayIntervalMs?: number) => {
-    ev: BaileysEventEmitter;
+    ev: WileysEventEmitter;
     task: Promise<void>;
 };
